afe56b2bd559f84f02ce4f707f663e9c50e92628
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2022-03-16 13:49:03 +0800

    🔧 UPDATE to use release-branch-semver as strategy

--- setup.py
+++ setup.py
@@ -18,4 +18,22 @@ version = __version__
 if not version:
     raise ValueError("Can't find the version in {name}/__init__.py")
 
-setup(use_scm_version=True)
+def myversion():
+    from setuptools_scm.version import (SEMVER_MINOR, guess_next_simple_semver,
+                                        release_branch_semver_version)
+
+    def my_release_branch_semver_version(version):
+        v = release_branch_semver_version(version)
+        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
+            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
+        return v
+
+    return {
+        'version_scheme': my_release_branch_semver_version,
+        'local_scheme': 'no-local-version',
+    }
+
+
+setup(use_scm_version=myversion)
+
+
